8d4f338580cd757de6314872bb37ffdfbb1a6426
Manoj Kumar <manoj@Manojs-MacBook-Air.local>
2025-01-28 15:21:12 +0000

    Fixed bug/Added new feature

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col,Button } from 'react-bootstrap';
 import '../App.css';
 import logo from '../assets/IZ-Logo.svg'
 import { Link } from 'react-router-dom'
